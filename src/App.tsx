f894d6a215e81801c51f5ff862b7d8ce13b841da
7cknmad <151736075+7cknmad@users.noreply.github.com>
2025-10-08 09:53:56 +0800

    Use HashRouter + set base for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import HomePage from './pages/HomePage';
 import MapExplorer from './pages/MapExplorer';
