05651d4b80bfbadd3776b366fcaac99b1a847e4a
7cknmad <151736075+7cknmad@users.noreply.github.com>
2025-10-08 16:31:28 +0800

    mockdata

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
   // 👇 must match your repo name EXACTLY (including caps)
   base: '/Bulacan-Mapping-Flavors/',
